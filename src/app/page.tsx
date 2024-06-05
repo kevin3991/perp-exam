6b73def301b5e13e282a2ad0e5bb6613def6b8e8
Sather33 <court.dream3@gmail.com>
2024-06-05 22:57:03 +0800

    feat: Add primereact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 // import Image from 'next/image';
+import { Button } from 'primereact/button';
 
 export default function Home(): JSX.Element {
-  return <div>Hello</div>;
+  return (
+    <div>
+      Hello
+      <Button label="Click" />
+    </div>
+  );
 }
